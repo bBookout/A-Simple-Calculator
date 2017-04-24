40ef25ecfee65ee46ed5e0a24365f867b2b6842c
Brian Bookout <brianbookout4@gmail.com>
2017-04-24 09:09:28 -0700

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 141px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <br />
+        <table border="1">
+        <tr>
+        <td class="auto-style1">
+        <br />
+            <div style="height: 154px; width: 137px; position: relative;">
+                <asp:TextBox ID="displayTextBox" runat="server" BackColor="Black" ForeColor="Lime" Style="margin-right: 7px" Width="123px"></asp:TextBox>
+
+                <br />
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="1" OnClick="Button1_Click" /><asp:Button ID="Button2" runat="server" Text="2" OnClick="Button2_Click" /><asp:Button ID="Button3" runat="server" Text="3" OnClick="Button3_Click" /><asp:Button ID="addButton" runat="server" Text="+" Width="21px" OnClick="addButton_Click" /><asp:Button ID="clearButton" runat="server" Text="clear" Width="42px" OnClick="clearButton_Click" />
+                <br />
+                <asp:Button ID="Button4" runat="server" Text="4" OnClick="Button4_Click" /><asp:Button ID="Button5" runat="server" Text="5" OnClick="Button5_Click" /><asp:Button ID="Button6" runat="server" Text="6" OnClick="Button6_Click" /><asp:Button ID="subtractButton" runat="server" Text="-" Width="21px" OnClick="subtractButton_Click" /><asp:Button ID="deleteButton" runat="server" Text="del" Width="42px" OnClick="deleteButton_Click" />
+                <br />
+                <asp:Button ID="Button7" runat="server" Text="7" OnClick="Button7_Click" /><asp:Button ID="Button8" runat="server" Text="8" OnClick="Button8_Click" /><asp:Button ID="Button9" runat="server" Text="9" OnClick="Button9_Click" /><asp:Button ID="multiplyButton" runat="server" Text="x" Width="21px" OnClick="multiplyButton_Click" />
+                <asp:Button ID="equalsButton" runat="server" Text="=" OnClick="equalsButton_Click" Width="42px" />
+                <br />
+                <asp:Button ID="Button0" runat="server" Text="0" OnClick="Button0_Click" Width="21px" />
+                <asp:Button ID="divideButton" runat="server" Text="/" Width="42px" OnClick="divideButton_Click" />
+            </div>
+            </td>
+            </tr>
+            </table>
+        <br />
+
+        <div>
+            <h1 id="instruction" runat="server">Please enter an addition, subtraction, multiplication, or division problem.</h1>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //no real need for any code or declarations on page load
+    }
+
+    void Application_Error(object sender, EventArgs e)
+    {
+        Exception exc = Server.GetLastError();
+
+        if (exc is HttpUnhandledException)
+        {
+            // A way to inform the user that an error has occurred
+            instruction.InnerText = "An error has occurred, please try again." + exc;
+
+        }
+        // Clear the error from the server.
+        Server.ClearError();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button1.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button2.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button3.Text;//onClick make the textBox display the number
+    }
+
+    protected void addButton_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += addButton.Text;//onClick make the textBox display the addition symbol
+    }
+
+    protected void clearButton_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text = "";//clear the textBox
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button4.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button5.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button6.Text;//onClick make the textBox display the number
+    }
+
+    protected void subtractButton_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += subtractButton.Text;//onClick make the textBox display the subtraction symbol
+    }
+
+    protected void deleteButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            displayTextBox.Text = displayTextBox.Text.Remove(displayTextBox.Text.Length - 1, 1);//delete a number or backspace 
+        }
+        catch(Exception exc)//attempt to catch an exception that might occur and inform the user
+        {
+            instruction.InnerText = "An error has occurred, please try again." + exc;
+        } 
+        finally
+        {
+            // Clear the error from the server.
+            Server.ClearError();
+        }  
+               
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button7.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button8.Text;//onClick make the textBox display the number
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button9.Text;//onClick make the textBox display the number
+    }
+
+    protected void multiplyButton_Click(object sender, EventArgs e)
+    {
+        int x;
+        x = Convert.ToInt16(displayTextBox.Text);
+        displayTextBox.Text += "*";     //onClick make the textBox display the multiplication symbol  
+    }
+
+    protected void equalsButton_Click(object sender, EventArgs e)
+    {
+        int total;//a variable for the operation's total
+        string number = displayTextBox.Text;//a variable for the text in the textbox that the user has entered
+        string[] numbers = number.Split('+', '*', '-', '/');//splits the text in number into an array to find which operator is present
+        if (number.Contains("+"))
+        {
+            total = Convert.ToInt16(numbers[0]) + Convert.ToInt16(numbers[1]);//performs the addition operation
+            displayTextBox.Text = total.ToString();//displays the total as a string in the textbox
+        }
+        if (number.Contains("*"))
+        {
+            total = Convert.ToInt16(numbers[0]) * Convert.ToInt16(numbers[1]);//performs the multiplication operation
+            displayTextBox.Text = total.ToString();//displays the total as a string in the textbox
+        }
+        if (number.Contains("-"))
+        {
+            total = Convert.ToInt16(numbers[0]) - Convert.ToInt16(numbers[1]);//performs the subtraction operation
+            displayTextBox.Text = total.ToString();//displays the total as a string in the textbox
+        }
+        if (number.Contains("/"))
+        {
+            total = Convert.ToInt16(numbers[0]) / Convert.ToInt16(numbers[1]);//performs the division operation
+            displayTextBox.Text = total.ToString();//displays the total as a string in the textbox
+        }
+    }
+
+    protected void Button0_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += Button0.Text;//onClick make the textBox display the number
+    }
+
+    protected void divideButton_Click(object sender, EventArgs e)
+    {
+        displayTextBox.Text += divideButton.Text;//onClick make the textBox display the division symbol
+    }
+
+    //protected void ptButton_Click(object sender, EventArgs e)
+    //{
+    //    displayTextBox.Text += ptButton.Text;
+    //}
+ 
+   
+}
